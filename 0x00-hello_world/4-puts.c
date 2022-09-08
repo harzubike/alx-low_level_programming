e184a697e76641b6313c10dabf987704256e5871
harzubike <harzubike@gmail.com>
2022-09-08 06:05:32 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function
+*main - print the string in the put function
 *
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
 */
 int main(void)
 {
