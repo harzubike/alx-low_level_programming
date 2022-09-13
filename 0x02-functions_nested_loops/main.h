d073d06f17ef3ff1aa70a62b660e501a5a75af44
harzubike <harzubike@gmail.com>
2022-09-13 15:28:08 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,20 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define HOLBERTON_H_INCLUDED
 
-int_putchar(char);
+/**
+*File:main.h
+*Author:Azubuike Uliah
+*Desc:header file containing functions declaration
+*/
+
 void print_alphabet(void);
+void jack_bauer(void);
 void print_alphabet_x10(void);
+void times_table();
+void print_to_98(int);
+void print_times_table(int);
+int _putchar(char);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-void num(int r, int n);
-
-#endif
+int add(int n, int m);
+int print_last_digit(int n);
