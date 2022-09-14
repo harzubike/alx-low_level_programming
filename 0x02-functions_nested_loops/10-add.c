8559a32a7e93f21f660b9c180fec244fa686f5db
harzubike <harzubike@gmail.com>
2022-09-14 14:12:08 -0700

    add c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * add - function that adds two integers
+  *
+  * @x: parameter x(int)
+  * @y: parameter y (int)
+  *
+  * Return: addition of two numbers
+  *
+  */
+int add(int x, int y)
+{
+	return (x + y);
+}
