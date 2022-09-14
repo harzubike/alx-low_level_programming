0f273fbd36b275bffe1b4c3b32ea127447240974
harzubike <harzubike@gmail.com>
2022-09-14 14:07:00 -0700

    24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,46 +1,42 @@
 #include "main.h"
 
 /**
-* jack_bauer - prints every minute of the day
-*
-* Description: prints every minute of the day
-*
-* Return: void
-*/
-
+  * jack_bauer -prints time count
+  *
+  */
 void jack_bauer(void)
 {
-int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
-hours_max = 58;
-hours_tens = '0';
-while (hours_tens < '3')
-{
-if (hours_tens == '2')
-{
-hours_max = '4';
-}
-hours_ones = '0';
-while (hours_ones < hours_max)
-{
-minutes_tens = '0';
-while (minutes_tens < '6')
-{
-minutes_ones = '0';
-while (minutes_ones < 58)
-_putchar(hours_tens);
-_putchar(hours_ones);
-_putchar(':');					
-_putchar(minutes_tens);
-_putchar(minutes_ones);
-_putchar('\n');
-minutes_ones++;
-}
-minutes_ones = '0';
-minutes_tens++;
-}
-minutes_tens = '0';
-hours_ones++;
-}
-hours_ones = '0';
-hours_tens++;
+	int hr1, hr2, min1, min2;
+	int t = 9;
+
+	hr2 = 0;
+	while (hr2 <= 2)
+	{
+		if (hr2 == 2)
+		{
+			t = 3;
+		}
+		hr1 = 0;
+		while (hr1 <= t)
+		{
+			min2 = 0;
+			while (min2 <= 5)
+			{
+				min1 = 0;
+				while (min1 <= 9)
+				{
+					_putchar('0' + hr2);
+					_putchar('0' + hr1);
+					_putchar(':');
+					_putchar('0' + min2);
+					_putchar('0' + min1);
+					_putchar('\n');
+					min1++;
+				}
+				min2++;
+			}
+			hr1++;
+		}
+		hr2++;
+	}
 }
