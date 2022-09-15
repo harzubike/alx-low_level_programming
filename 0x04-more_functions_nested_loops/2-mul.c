02d3ca0ff1dc39732aeb3b7680beb9a7c76a2294
harzubike <harzubike@gmail.com>
2022-09-15 08:05:28 -0700

    mul c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * mul - is a function that checks if character alphabet
+  *
+  * @a: stores value for first parameter
+  * @b: stored value for second parameter
+  *
+  * Return: Always return 0 or 1 on success
+  *
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
