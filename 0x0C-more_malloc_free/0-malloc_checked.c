02f215f9efea1127a295259f79a0449cd5a34ff8
harzubike <harzubike@gmail.com>
2022-10-06 14:44:45 -0700

    malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,21 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * malloc_checked - uses malloc to assign dynamic memory
- * @b: size of memory to allocate
- *
- * Return: void
+ * malloc_checked - Creates an array based on passed in value
+ * @b: Value passed in for array creation
+ * Return: nothing
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
+	void *ptr = NULL;
 
 	ptr = malloc(b);
+
 	if (ptr == NULL)
-	{
 		exit(98);
-	}
 
 	return (ptr);
 }
