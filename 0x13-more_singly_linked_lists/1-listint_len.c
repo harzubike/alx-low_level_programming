6daff78794f21d266202a7532561ddae10e8454a
harzubike <harzubike@gmail.com>
2022-10-24 13:29:08 -0700

    more singly linked tasks

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked lists
- * @h: linked list of type listint_t to traverse
+ * listint_len - counts the number of nodes in a linked list
+ * @h: head of the list
  *
- * Return: number of nodes
+ * Return: the number of elements
  */
 size_t listint_len(const listint_t *h)
 {
-    size_t num = 0;
+	const listint_t *cursor = h;
+	size_t count = 0;
 
-    while (h)
-    {
-        num++;
-        h = h->next;
-    }
-
-    return (num);
+	while (cursor != NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
 }
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,23 +1,26 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the beginning of a linked list
- * @head: pointer to the first node in the list
- * @n: data to insert in that new node
+ * add_nodeint - adds a node to the beginning of a linked list
+ * @head: pointer to the head of the list
+ * @n: integer to be used as content
  *
- * Return: pointer to the new node, or NULL if it fails
+ * Return: address of the newly added node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new_node;
 
-    new = malloc(sizeof(listint_t));
-    if (!new)
-        return (NULL);
-
-    new->n = n;
-    new->next = *head;
-    *head = new;
-
-    return (new);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node != NULL)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+	}
+	else
+		return (NULL);
+	if (*head != NULL)
+		new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
